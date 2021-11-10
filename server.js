954871ca064656bee76bffacb5dfedd573050e1e
Thuc Do <hoikhongtuyen@example.com>
2021-11-10 10:58:10 +0700

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 const {sequelize} = require('./models')
 const {rootRouter} = require('./routers');
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.use( express.static(path.join(__dirname, './public')));
+
+app.use(cors());
 
 app.use('/api', rootRouter);  
 
