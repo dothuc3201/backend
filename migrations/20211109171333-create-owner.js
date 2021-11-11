767149018ae2242000ddf432c78543fe1ae3f941
Thuc Do <hoikhongtuyen@example.com>
2021-11-11 11:43:09 +0700

    fix

---
+++ migrations/20211109171333-create-owner.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Owners', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      user_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+        references: {
+          // This is a reference to another model
+          model: 'users',
+    
+          // This is the column name of the referenced model
+          key: 'id'
+        }
+        
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Owners');
+  }
+};
